161c63862b8eb44a39e1237c3263d443b99afeb8
henryandesjr <henryandesjr@gmail.com>
2023-09-06 10:23:11 +0800

    Created models

---
+++ AspNetCoreMVC-Ecommerce/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetCoreMVC_Ecommerce.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror
+    }
+}
